9c3fdf419998447d5e653dfe74f2b4d6ca604ecc
GilCasRam <gcasimiror1000@egresado.ipn.mx>
2025-01-29 14:04:59 -0600

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Gil Alfredo Casimiro Ramírez on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
